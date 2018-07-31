8f6a94ca0f79ee53f0df707d270de43f2a832adf
Thomas Sutton <me@thomas-sutton.id.au>
2018-07-31 21:26:18 +1000

    Remove recursion schemes

--- zygo.hs
+++
@@ -1,8 +0,0 @@
-zygoHistoPrepro
-  :: (Corecursive t, Recursive t)
-  => (Base t b -> b)
-  -> (forall c. Base t c -> Base t c)
-  -> (Base t (EnvT b (Cofree (Base t)) a) -> a)
-  -> t
-  -> a
-zygoHistoPrepro f g t = gprepro (distZygoT f distHisto) g t
